997cfdb88e94b741978cd3cf658c62b20478f4f4
Benny Zlotnik <bzlotnik@redhat.com>
2020-05-14 18:36:01 +0300

    refactor code
    - move config out of main
    - move file reading out of main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
 use std::env;
 use std::fs;
+use std::{process, error::Error};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let query = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Couldn't parse the arguments {}", err);
+        process::exit(1)
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-    
-    let content = fs::read_to_string(filename)
-        .expect("Failed to read the file");
-    println!("With content: {}", content);
+    println!("Running query {}", config.query);
+    println!("In file: {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Failed to read content! {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+    println!("With text: {}", content);
+
+    // This is like a void function, but it can also return errors!
+    Ok(())
+}
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments provided");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        return Ok(Config { query, filename });
+    }
 }
