90011b33f330fa44f76b4a7932008a6af95573ab
Benny Zlotnik <uber442@gmail.com>
2020-05-16 12:15:02 +0300

    support case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
+    
+    let result = if config.case_sensitive {
+        search(&config.query, &content)
+    } else {
+        search_case_insensitive(&config.query, &content)
+    };
 
-    for line in search(&config.query, &content) {
+    for line in result {
         println!("{}", line);
     }
 
@@ -14,6 +21,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -24,8 +32,12 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
-
-        return Ok(Config { query, filename });
+        let case_sensitive  = env::var("CASE_INSENSITIVE").is_err();
+        return Ok(Config { 
+            query,
+            filename,
+            case_sensitive, 
+        });
     }
 }
 
@@ -33,6 +45,14 @@ pub fn search<'a>(query: &'a str, content:  &'a str) -> Vec<&'a str> {
     content
         .lines()
         .filter(|line| line.contains(query))
+        .collect::<Vec<&str>>()
+}
+
+pub fn search_case_insensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase(); // query is now a String, not a slice
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
         .collect::<Vec<&str>>()
 }
 
@@ -49,5 +69,16 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, proDuctive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, proDuctive."], search_case_insensitive(query, content));
     }
 }
